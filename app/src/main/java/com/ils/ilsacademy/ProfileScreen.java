f26d7b7beffc350cd6f291df6fca7a9d9788178f
hiten0612 <phiten612@gmail.com>
2021-02-28 01:06:44 +0530

    add fonts

--- app/src/main/java/com/ils/ilsacademy/ProfileScreen.java
+++ app/src/main/java/com/ils/ilsacademy/ProfileScreen.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
